9c3c5df75b394ed3d37e2d23b2e597f857710f9b
Mareike Wendler <49586574+MareikeW@users.noreply.github.com>
2021-08-17 08:55:10 +0200

    Styling + Komponenten und Service angelegt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GoalFormComponent } from './goal-form/goal-form.component';
+import { GoalsDisplayComponent } from './goals-display/goals-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoalFormComponent,
+    GoalsDisplayComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/goals-display/goals-display.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-goals-display',
+  templateUrl: './goals-display.component.html',
+  styleUrls: ['./goals-display.component.scss']
+})
+export class GoalsDisplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
